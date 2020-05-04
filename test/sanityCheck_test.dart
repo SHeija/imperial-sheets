8051ea82c2fe8b72d5ae341fc87b4845162682a1
SHeija <SHeija@pm.me>
2020-05-04 21:47:51 +0300

    attempt 2

--- test/sanityCheck_test.dart
+++ test/sanityCheck_test.dart
@@ -6,43 +6,43 @@ import 'package:imperial_sheets/views/mainView.dart';
 import 'package:imperial_sheets/views/noCharacterView.dart';
 
 void main() {
-  final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+  //final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
   group('SanityCheck', () {
     testWidgets('Views update when information is changed',
         (WidgetTester tester) async {
-      await binding.setSurfaceSize(Size(1000, 800));
+      //await binding.setSurfaceSize(Size(1000, 800));
       final widget = App();
       await tester.pumpWidget(widget);
-      await tester.pumpAndSettle(Duration(seconds: 2));
+      await tester.pump();
       // nothing
       expect(find.byType(NoCharacterView), findsOneWidget);
 
       // open drawer
       await tester.tap(find.byIcon(Icons.menu));
-      await tester.pumpAndSettle(Duration(seconds: 2));
+      await tester.pumpAndSettle();
       expect(find.byType(Drawer), findsOneWidget);
 
       // add character
       await tester.tap(find.byIcon(Icons.add));
-      await tester.pumpAndSettle(Duration(seconds: 2));
-
+      await tester.pumpAndSettle();
       // assert that the new character is shown
       expect(find.byType(MainView), findsOneWidget);
       expect(find.text('Description'), findsOneWidget);
-      expect(find.byType(NoCharacterView), findsNothing);
+      expect(find
+          .byType(NoCharacterView), findsNothing);
 
       // edit info
       expect(find.text('40'), findsNothing);
       await tester.longPress(find.text('Strength'));
-      await tester.pumpAndSettle(Duration(seconds: 2));
+      await tester.pumpAndSettle();
       await tester.enterText(find.byType(FormBuilderTextField), '40');
       await tester.tap(find.text('Submit'));
-      await tester.pumpAndSettle(Duration(seconds: 2));
+      await tester.pumpAndSettle();
       expect(find.text('40'), findsOneWidget);
 
       // assert that new character is in drawer menu
       await tester.tap(find.byIcon(Icons.menu));
-      await tester.pumpAndSettle(Duration(seconds: 2));
+      await tester.pumpAndSettle();
       expect(find.text('Name'), findsOneWidget);
     });
   });
