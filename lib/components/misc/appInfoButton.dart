a41a4a1ef49e67f86380337918c4741655b7c38b
= <sheija@pm.me>
2020-08-15 20:04:43 +0300

    automatic version number in drawer

--- lib/components/misc/appInfoButton.dart
+++ lib/components/misc/appInfoButton.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
 
 class AppInfoButton extends StatelessWidget {
+
+  Future<String> _getVersionNumber() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    return packageInfo.version;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(Icons.info_outline),
-      title: Text('Version'),
-      subtitle: Text('0.0.3'),
+    return FutureBuilder(
+      future: _getVersionNumber(),
+      builder: (context, snapshot) {
+        return ListTile(
+          leading: Icon(Icons.info_outline),
+          title: Text('Version'),
+          subtitle: snapshot.hasData ? Text(snapshot.data) : null,
+        );
+      },
     );
   }
 }
---
+++ lib/components/misc/menuDrawer.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:imperial_sheets/database/hiveProvider.dart';
+import 'package:imperial_sheets/models/character.dart';
+
+import 'appInfoButton.dart';
+import 'importButton.dart';
+
+class MenuDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget _drawerCharacterListBuilder() {
+      return ValueListenableBuilder(
+        valueListenable: HiveProvider.of(context).characters.listenable(),
+        builder: (context, box, widget) {
+          List<Character> _characterList = box.values.toList();
+          return SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return ListTile(
+                  onTap: () {
+                    HiveProvider.of(context)
+                        .settings
+                        .put('activeCharacter', _characterList[index].id);
+                    print('Set active character ' + _characterList[index].id);
+                    Navigator.of(context).pop();
+                  },
+                  leading: Icon(Icons.bookmark),
+                  title: Text(_characterList[index].name),
+                );
+              },
+              childCount: _characterList.length,
+            ),
+          );
+        },
+      );
+    }
+
+    Widget _drawerActionsBuilder() {
+      return SliverList(
+        delegate: SliverChildListDelegate([
+          Divider(),
+          ListTile(
+            title: Text('Add new character'),
+            leading: Icon(Icons.add),
+            onTap: () {
+              Character _newChar = Character.blank()
+                ..id = DateTime.now().toIso8601String();
+              HiveProvider.of(context).characters.put(_newChar.id, _newChar);
+              HiveProvider.of(context)
+                  .settings
+                  .put('activeCharacter', _newChar.id);
+              print('created character ' + _newChar.id);
+              Navigator.of(context).pop();
+            },
+          ),
+          ImportButton(),
+          AppInfoButton(),
+        ]),
+      );
+    }
+
+    return Drawer(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverPadding(
+            padding: EdgeInsets.zero,
+            sliver: SliverToBoxAdapter(
+              child: DrawerHeader(
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text('Imperial Sheets',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          .copyWith(color: Colors.white)),
+                ),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.zero,
+            sliver: _drawerCharacterListBuilder(),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.zero,
+            sliver: _drawerActionsBuilder(),
+          ),
+        ],
+      ),
+    );
+  }
+}
