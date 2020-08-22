466171be4385c4174945781e8842ae7de288e3eb
= <sheija@pm.me>
2020-08-22 22:02:31 +0300

    fix test harder

--- test/widget_tests/tiles/appInfoButton_test.dart
+++ test/widget_tests/tiles/appInfoButton_test.dart
@@ -20,7 +20,11 @@ void main() {
 
     testWidgets('tap opens a dialog', (WidgetTester tester) async {
       final Widget widget = MaterialApp(
-        home: AppInfoButton(),
+        home: Scaffold(
+          body: Container(
+            child: AppInfoButton(),
+          ),
+        ),
       );
 
       await tester.pumpWidget(widget);
